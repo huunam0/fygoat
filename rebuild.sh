9165c185038e886ea81b0eb997e94448cba0ef77
HuuNam0 <HuuNam0@gmail.com>
2012-10-13 02:19:34 +0000

    Up

--- rebuild.sh
+++ rebuild.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-service footygoat status
-service footygoat stop
-ps -ef | grep gmatch
-kill `ps -ef | grep gmatch | grep -v grep | awk '{print $2}'`
+#service footygoat status
+#service footygoat stop
+#ps -ef | grep gmatch
+#kill `ps -ef | grep gmatch | grep -v grep | awk '{print $2}'`
 for i in $*; do
    echo Building  $i
    rm $i
    make $i
-   cp $i /usr/bin
+   sudo cp $i /usr/bin
  done
-service footygoat start
+#service footygoat start
