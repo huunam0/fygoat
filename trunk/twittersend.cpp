519bbb4a1d222f81df98725073ed548e3fb12044
HuuNam0@gmail.com <HuuNam0@gmail.com@19e86b3e-7b26-1824-6d2d-5a83d6761a0b>
2012-11-30 03:05:59 +0000

    Sua loi luu file

--- trunk/twittersend.cpp
+++ trunk/twittersend.cpp
@@ -76,6 +76,8 @@ bool init_conf()
 {
 	FILE *fp=NULL;
 	char buf[BUFFER_SIZE];
+    char user_name[BUFFER_SIZE];
+    char password [BUFFER_SIZE];
     char consumerkey [BUFFER_SIZE];
     char consumersecret [BUFFER_SIZE];
     char token[BUFFER_SIZE];
@@ -162,7 +164,7 @@ void printUsage()
 
 bool initTwitter()
 {
-    char tmpBuf[1024];
+    //char tmpBuf[1024];
     twitterObj.setTwitterUsername(t_name);
     twitterObj.setTwitterPassword(t_pass);
     twitterObj.getOAuth().setConsumerKey(t_key);
@@ -171,9 +173,11 @@ bool initTwitter()
     std::string myOAuthAccessTokenSecret("");
     std::ifstream oAuthTokenKeyIn;
     std::ifstream oAuthTokenSecretIn;
-
-    oAuthTokenKeyIn.open( "twitterClient_token_key.txt" );
-    oAuthTokenSecretIn.open( "twitterClient_token_secret.txt" );
+    char token_key[]="twittersend_token_key.txt";
+    char token_secret[]="twittersend_token_secret.txt";
+    oAuthTokenKeyIn.open(token_key);
+    oAuthTokenSecretIn.open(token_secret);
+    char tmpBuf[1024];
 
     memset( tmpBuf, 0, 1024 );
     oAuthTokenKeyIn >> tmpBuf;
@@ -187,6 +191,10 @@ bool initTwitter()
     oAuthTokenSecretIn.close();
     if( myOAuthAccessTokenKey.size() && myOAuthAccessTokenSecret.size() )
     {
+        /* If we already have these keys, then no need to go through auth again */
+        if (DEBUG)
+            printf( "\nUsing:\nKey: %s\nSecret: %s\n\n", myOAuthAccessTokenKey.c_str(), myOAuthAccessTokenSecret.c_str() );
+
         twitterObj.getOAuth().setOAuthTokenKey( myOAuthAccessTokenKey );
         twitterObj.getOAuth().setOAuthTokenSecret( myOAuthAccessTokenSecret );
     }
@@ -195,6 +203,12 @@ bool initTwitter()
         std::string authUrl;
         twitterObj.oAuthRequestToken( authUrl );
         twitterObj.oAuthHandlePIN( authUrl );
+
+
+        /* Step 4: Exchange request token with access token */
+        twitterObj.oAuthAccessToken();
+
+        /* Step 5: Now, save this access token key and secret for future use without PIN */
         twitterObj.getOAuth().getOAuthTokenKey( myOAuthAccessTokenKey );
         twitterObj.getOAuth().getOAuthTokenSecret( myOAuthAccessTokenSecret );
 
@@ -202,8 +216,8 @@ bool initTwitter()
         std::ofstream oAuthTokenKeyOut;
         std::ofstream oAuthTokenSecretOut;
 
-        oAuthTokenKeyOut.open( "twitterClient_token_key.txt" );
-        oAuthTokenSecretOut.open( "twitterClient_token_secret.txt" );
+        oAuthTokenKeyOut.open(token_key);
+        oAuthTokenSecretOut.open(token_secret);
 
         oAuthTokenKeyOut.clear();
         oAuthTokenSecretOut.clear();
@@ -214,7 +228,7 @@ bool initTwitter()
         oAuthTokenKeyOut.close();
         oAuthTokenSecretOut.close();
         /* Step 2: Get request token key and secret */
-        return false;
+
     }
     /* OAuth flow ends */
 
