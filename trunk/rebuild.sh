bc1399ff58213096db109a6137fa281c1d4e59d8
HuuNam0@gmail.com <HuuNam0@gmail.com@19e86b3e-7b26-1824-6d2d-5a83d6761a0b>
2012-12-03 08:39:54 +0000

    Sua loi segmentation fault Sua file ftriggers

--- trunk/rebuild.sh
+++ trunk/rebuild.sh
@@ -5,8 +5,9 @@
 #kill `ps -ef | grep gmatch | grep -v grep | awk '{print $2}'`
 for i in $*; do
    echo -e "\e[0;31m Building\e[0m  $i"
-   rm $i
+   rm -f $i
    make $i
+   echo -e "\e[0;31m Copy binary to /usr/bin \e[0m  "
    sudo cp $i /usr/bin
  done
 #service footygoat start
