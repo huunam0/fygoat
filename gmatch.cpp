2c48db8f5a196f75a0dfccabdadc1a53988f3401
HuuNam0 <HuuNam0@gmail.com>
2013-02-23 15:36:01 +0000

    debug

--- gmatch.cpp
+++ gmatch.cpp
@@ -580,7 +580,7 @@ bool getMatch(int id) //for major league
     }
     if (!t.isEmpty()) t=sh.cutTagByName("section");
     if (!t.isEmpty()) if (!t.contain("Yellow Cards")) t=sh.cutTagByName("section");
-    if (DEBUG) cout<<"t:: "<<t.getContent().substr(1,150)<<endl;
+    if (DEBUG) if (!t.isEmpty()) cout<<"t:: "<<t.getContent().substr(1,150)<<endl;
     if (t.contain("Yellow Cards"))
     {
         for(i=0;i<2;i++)
@@ -787,17 +787,6 @@ bool getMatch0(int id) //for little league
         }
 
         t=sh.cutTagByName("section");
-    }
-    if (!t.isEmpty()) t=sh.cutTagByName("section");
-    if (!t.isEmpty()) if (!t.contain("Yellow Cards")) t=sh.cutTagByName("section");
-    if (t.contain("Yellow Cards"))
-    {
-        for (i=0; i<2; i++)
-        {
-            n=t.cutTagByName("div");
-            v[i]=n.count("soccer-icons-yellowcard");
-        }
-
     }
     /*
     if (reCard) {
