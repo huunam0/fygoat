df707e3825b6f6b822589585508375e41ec2424c
HuuNam0 <HuuNam0@gmail.com>
2014-07-14 02:48:53 +0000

    Update gmatch

--- gmatch.cpp
+++ gmatch.cpp
@@ -10,7 +10,7 @@ static char user_name[BUFFER_SIZE];
 static char password [BUFFER_SIZE];
 static char db_name  [BUFFER_SIZE];
 static int port_number;
-static int DEBUG=0;
+static int DEBUG=false;
 
 static MYSQL *conn;
 
@@ -110,7 +110,11 @@ bool init_mysql_conf()
     }
 }
 bool executesql(const char * sql){
-
+    if (DEBUG)
+    {
+        cout<<"SQL: "<<sql<<endl;
+        return true;
+    }
 	if (mysql_real_query(conn,sql,strlen(sql)))
     {
 		//sleep(20);
@@ -140,63 +144,16 @@ int init_mysql() {
         return false;
 	return true;
 }
-void emitEvent(int iIndex, int iValue, int iTeam)
-{
-    char sql[300];
-    if (DEBUG)
-        cout<<"Event "<<iIndex<<" value: "<<iValue<<" with team "<<iTeam<<endl;
-    //sprintf(sql,"INSERT IGNORE INTO f_timeline (`event`, `value`, `team`, `match`, `date`) VALUE (%d,%d,%d,%d,NOW())",iIndex,iValue,iTeam,mid);
-    sprintf(sql,"INSERT INTO f_timeline (`event`, `value`, `team`, `match`, `date`) VALUE (%d,%d,%d,%d,NOW()) ON DUPLICATE KEY UPDATE `value`=%d,`date`=NOW(); ",iIndex,iValue,iTeam,mid,iValue);
-    executesql(sql);
-    string t,field;
-    if (iIndex<8)
-        t=(iTeam==1?"a":"h");
-    switch (iIndex)
-    {
-    case 0:
-        field="goals";
-        break;
-    case 1:
-        field="1goals";
-        break;
-    case 2:
-        field="reds";
-        break;
-    case 3:
-        field="yellows";
-        break;
-    case 4:
-        field="shots";
-        break;
-    case 5:
-        field="gshots";
-        break;
-    case 6:
-        field="corner";
-        break;
-    case 7:
-        field="possession";
-        break;
-    case 8:
-        field="minutes";
-        break;
-    case 9:
-        field="status";
-        break;
-    }
-    if (!field.empty())
-    {
-        sprintf(sql,"UPDATE f_matches SET %s=%d where match_id=%d",(t+field).c_str(),iValue,mid);
-        //cout<<sql<<endl;
-        executesql(sql);
-    }
 
-}
 void emit2Event(int iIndex, int iValue0, int iValue1)
 {
     char sql[300];
     if (DEBUG)
+    {
         cout<<"Event "<<iIndex<<" value: "<<iValue0<<" - "<<iValue1<<endl;
+        return;
+    }
+
     //sprintf(sql,"INSERT IGNORE INTO f_timeline (`event`, `value`, `team`, `match`, `date`) VALUE (%d,%d,%d,%d,NOW())",iIndex,iValue,iTeam,mid);
     sprintf(sql,"INSERT INTO f_timeline2 (`event`, `home`, `away`, `match`, `date`) VALUE (%d,%d,%d,%d,NOW()) ON DUPLICATE KEY UPDATE `home`=%d,`away`=%d,`date`=NOW(); ",iIndex,iValue0,iValue1,mid,iValue0,iValue1);
     executesql(sql);
@@ -245,15 +202,8 @@ void emit2Event(int iIndex, int iValue0, int iValue1)
     }
 
 }
-void setEvent(int iEvent,int iValue)
-{
-    char sql[300];
-    //sprintf(sql,"INSERT IGNORE INTO f_timeline (`event`, `value`, `team`, `match`, `date`) VALUE (%d,%d,%d,%d,NOW())",iEvent,iValue,0,mid);
-    sprintf(sql,"INSERT INTO f_timeline (`event`, `value`, `team`, `match`, `date`) VALUE (%d,%d,%d,%d,NOW()) ON DUPLICATE KEY UPDATE `value`=%d,`date`=NOW();",iEvent,iValue,0,mid,iValue);
-    executesql(sql);
 
-}
-void setEvent2(int iEvent,int iValue0=0,int iValue1=0)
+void setEvent2(int iEvent,int iValue0=0,int iValue1=0) //special events
 {
     char sql[300];
     //sprintf(sql,"INSERT IGNORE INTO f_timeline (`event`, `value`, `team`, `match`, `date`) VALUE (%d,%d,%d,%d,NOW())",iEvent,iValue,0,mid);
@@ -261,18 +211,14 @@ void setEvent2(int iEvent,int iValue0=0,int iValue1=0)
     executesql(sql);
 
 }
-void setValue(int iIndex, int iValue=0, int iTeam=0)
-{
-    //if ( (isFirstTime) || ((m[iTeam][iIndex]<iValue)&&(iIndex!=7)) || ((m[iTeam][iIndex]!=iValue)&&(iIndex==7)))
-    if ( (isFirstTime) ||  ((m[iTeam][iIndex]!=iValue)))
-    {
-        m[iTeam][iIndex]=iValue;
-        emitEvent(iIndex,iValue,iTeam);
-    }
-}
+
 void set2Value(int iIndex, int iValue0=0, int iValue1=0)
 {
-    //if ( (isFirstTime) || ((m[iTeam][iIndex]<iValue)&&(iIndex!=7)) || ((m[iTeam][iIndex]!=iValue)&&(iIndex==7)))
+    if (DEBUG)
+    {
+        cout<<"EMIT #"<<iIndex<<": "<<iValue0<<"-"<<iValue1<<endl;
+        return;
+    }
     if ( (isFirstTime) ||  ((m[0][iIndex]!=iValue0)) || (m[1][iIndex]!=iValue1))
     {
         m[0][iIndex]=iValue0;
@@ -280,54 +226,7 @@ void set2Value(int iIndex, int iValue0=0, int iValue1=0)
         emit2Event(iIndex,iValue0,iValue1);
     }
 }
-int parseStatus( shtml status)
-{
-    //int v = status
-    if (status.contain("Full-time"))
-    {
-        return 7;
-    }
-    else if (status.contain("Match"))
-    {
-        return 0;
-    }
-    else if (status.contain("First Half"))
-    {
-        return 1;
-    }
-    else if (status.contain("Half-time"))
-    {
-        return 2;
-    }
-    else if (status.contain("Second Half"))
-    {
-        return 3;
-    }
-    else if (status.contain("Final score"))
-    {
-        if (status.contain("after extra time"))
-        {
-            return 8;
-        }
-        else return 9;
-    }
-    else if (status.contain("Postponed"))
-    {
-        return 11;
-    }
-    else if (status.contain("Suspended"))
-    {
-        return 6;
-    }
-    else if (status.contain("Abandoned"))
-    {
-        return 10;
-    }
-    else
-    {
-        return 1;
-    }
-}
+
 void parseStatus2(shtml stat)
 {
     if (stat.contain("FT-Pens"))
@@ -390,15 +289,23 @@ void parseStatus2(shtml stat)
         }
     }
 }
+void addTeam(string teamid, string tname, string league,string group)
+{
+    char sql[1000];
+    sprintf(sql,"INSERT INORGE INTO f_teams (team_id,team_name,team_league,team_group,team_date,team_updated) VALUE (%s,'%s','%s','%s',NOW(),0) ON DUPLICATE KEY UPDATE team_name='%s',team_league='%s',team_group='%s',team_date=NOW(),team_updated=0;",teamid.c_str(),tname.c_str(),league.c_str(),group.c_str(),tname.c_str(),league.c_str(),group.c_str());
+    //write_log("Add new team %d %s",teamid,tname.c_str());
+    executesql(sql);
+}
 bool getMatch(int id) //for major league
 {
     shtml sh,t,n,nh,nh2;
+    string hname, h_id, aname, a_id;
     int i=0,v[2],g[2];
+    int sc[2]={0,0};
+    int sc1[2]={0,0};
     char url[300];
     bool reCard = true;
-    sprintf(url,"http://espnfc.com/en/gamecast/statistics/id/%d/statistics.html?soccernet=true&cc=4716",id);
-    //sprintf(url,"http://soccernet.espn.go.com/match/_/id/%d?cc=4716",id);
-    //sprintf(url,"http://soccernet.espn.go.com/match?id=%d&cc=4716",id);
+    sprintf(url,"http://www.espnfc.com/gamecast/statistics/id/%d/statistics.html",id);
     if (DEBUG)
         cout<<"Loading from "<<url<<endl;
     if(!sh.loadFromURL(url))
@@ -410,86 +317,87 @@ bool getMatch(int id) //for major league
     /*Remove comments in html*/
     sh.removeBetween("<!--","-->",-1);
     /* Remove odd parts*/
-    t=sh.cutTagByName("div");
-    while (!t.isEmpty())
-    {
-        if (t.containAttr("content-wrap"))
-        {
-            sh=t;
-            break;
-        }
-        if (sh.isEmpty()) break;
-        t=sh.cutTagByName("div");
-    }
-
+    sh.retainTagByName("section");
     sh.retainTagByName("div");
-    if (DEBUG) cout<<"->MAIN:"<<sh.getContent().substr(1,200)<<endl;
-    //sh.retainTagByName("div");
-    //sh.retainTagByName("div",2);
-    //sh.retainTagByName("div");
-    //t=sh.cutTagByName("div",2);
-    //t.retainTagByName("div");
-    //n = t.cutTagByName("div");
+    sh.retainTagByName("div");
 
-    /*Get status and obmit event 8th*/
-    n.setContent(sh.getBetween("<p class=\"time\">","</p>"));
-    n.trim();
-    parseStatus2(n);
-    set2Value(8,status,minutes);
-    if (DEBUG)
-        cout<<"Status:"<<status<<endl;
-    //t.viewContent();
-    /* Get other infos */
-    if (sh.contain("id=\"matchstats\""))
+    t=sh.cutTagByName("section");
+    t.retainTagByName("section");
+    t.retainTagByName("div",3);
+    t.removeTagByName("div");
+    if (isFirstTime) //get teams
     {
-        if (DEBUG) cout<<"-> OLD VERSION"<<endl;
-        sh.retainTagByName("section",3);
-        sh.retainTagByName("div");
-        sh.retainTagByName("div");
+        n=t.cutTagByName("div");//away team
+        a_id=n.getBetweenAttr("teamId-","\"");
+        n.retainTagByName("p");
+        n.retainTagByName("a");
+        aname=n.getContent();
+        addTeam(a_id,aname,"","");
+        n=t.cutTagByName("div");//score & time
+
+
+        t.retainTagByName("div");//home team
+        h_id=t.getBetweenAttr("teamId-","\"");
+        t.retainTagByName("p");
+        t.retainTagByName("a");
+        hname=t.getContent();
+        addTeam(h_id,hname,"","");
+    }
+    else //only get time & status
+    {
+        n=t.cutTagByName("div",2);
+    }
+    n.retainTagByName("div");
+
+    nh=n.cutTagByName("p");//score
+    nh.trim();
+    sc[0]=nh.toInt();
+    if (nh.contain("v"))//has not start
+    {
+        status=0;
     }
     else
     {
-        if (DEBUG) cout<<"-> NEW VERSION"<<endl;
-        //sh.retainTagByName("div",2);
-        sh.removeTagByName("section");
-        sh.removeTagByName("section");
-        sh.removeTagByName("section");
-        sh.removeTagByName("div");
+        nh.deleteTo("-");
+        nh.trim();
+        sc[1]=nh.toInt();
+        status=1;
     }
-    if (DEBUG) cout<<"sh->"<<sh.getContent().substr(1,200)<<endl;
+    set2Value(0,sc[0],sc[1]);
+    nh=n.cutTagByName("p");
+    nh.trim();
+
+    parseStatus2(nh);
+    set2Value(8,status,minutes);
+    if (status==1) //first half
+    {
+        sc1[0]=sc[0];
+        sc1[1]=sc[1];
+    }
+    set2Value(1,sc1[0],sc1[1]);
+
+    t=sh.cutTagByName("section");//omit
     t=sh.cutTagByName("section");
-    if (DEBUG) cout<<"t->"<<t.getContent().substr(1,200)<<endl;
-    if (t.contain("Scoring Summary")) {
-        if (DEBUG) cout<<"->Scoring Summary"<<endl;
-        int sc[2]={0,0};
-        int sc1[2]={0,0};
-        for (i=0;i<2;i++)
-        {
-            nh=t.cutTagByName("tbody");
-            n=nh.cutTagByName("tr");
-            while (!n.isEmpty())
-            {
-                n.retainTagByName("td");
-                if (n.contain("'"))
-                {
-                    n.retainBetween("(","'");
-                    if (n.contain(" miss ")) continue;
-                    while (n.contain(" "))
-                        {
-                            n.deleteTo(" ");
-                        }
-                    v[i]=n.toInt();
-                    sc[i]++;
-                    if (v[i]<46) sc1[i]++;
-                }
-                n=nh.cutTagByName("tr");
-            }
-        }
-        set2Value(0,sc[0],sc[1]);
-        set2Value(1,sc1[0],sc1[1]);
-        t=sh.cutTagByName("section");
-    }
-    if (DEBUG) cout<<"t->"<<t.getContent().substr(1,200)<<endl;
+    t.retainTagByName("div");
+    t.retainTagByName("div");
+    n=t.cutTagByName("section");//score sumary
+    //process - not enought infos
+    n=t.cutTagByName("section");//omit
+    n=t.cutTagByName("section");//omit
+    n=t.cutTagByName("section");//yellow & red card(s)
+    nh=n.cutTagByName("div"); // home-yellow
+    nh.viewContent();
+    v[0]=nh.count("soccer-icons-yellowcard");
+    nh=n.cutTagByName("div");
+    v[1]=nh.count("soccer-icons-yellowcard");
+    set2Value(3,v[0],v[1]);
+    nh=n.cutTagByName("div"); // home-red
+    v[0]=nh.count("soccer-icons-redcard");
+    nh=n.cutTagByName("div");
+    v[1]=nh.count("soccer-icons-redcard");
+    set2Value(2,v[0],v[1]);
+
+    /*
     if (t.contain("Match Stats")) {
         if (DEBUG) cout<<"->Match Stats"<<endl;
         t.retainTagByName("table");
@@ -596,215 +504,6 @@ bool getMatch(int id) //for major league
         }
         set2Value(2,v[0],v[1]);
     }
-    /*
-    if (reCard) {
-        if (DEBUG) cout<<"->Recalcule red & yellow cards"<<endl;
-        for (i=0; i<2; i++)
-        {
-            t.removeTagByName("div",3);
-            //int card[3];
-            v[i]=t.count("soccer-icons-yellowcard");
-            //setValue(3,v,i);
-            g[i]=t.count("soccer-icons-redcard");
-            //setValue(2,v,i);
-            //cout<<"Team "<<i<<" has "<<card[1]<<" yellow card(s) and "<<card[2]<<" red card(s)."<<endl;
-            t=sh.cutTagByName("div");
-        }
-        set2Value(3,v[0],v[1]);
-        set2Value(2,g[0],g[1]);
-    }
-    */
-    isFirstTime=false;
-    //cout<<"End of get data"<<endl;
-    return true;
-}
-bool getMatch0(int id) //for little league
-{
-    shtml sh,t,n,nh,nh2;
-    int i=0,v[2],g[2];
-    char url[300];
-    bool reCard = true;
-    sprintf(url,"http://espnfc.com/en/gamecast/statistics/id/%d/statistics.html?soccernet=true&cc=4716",id);
-    //sprintf(url,"http://soccernet.espn.go.com/match/_/id/%d?cc=4716",id);
-    //sprintf(url,"http://soccernet.espn.go.com/match?id=%d&cc=4716",id);
-    if (DEBUG)
-        cout<<"Loading from "<<url<<endl;
-    if(!sh.loadFromURL(url))
-    {
-        if (DEBUG) cout<<"Cannot load from "<<url<<endl;
-        return false;
-    }
-    if (sh.isEmpty()) return false;
-    /*Remove comments in html*/
-    sh.removeBetween("<!--","-->",-1);
-    /* Remove odd parts*/
-    t=sh.cutTagByName("div");
-    while (!t.isEmpty())
-    {
-        if (t.containAttr("content-wrap"))
-        {
-            sh=t;
-            break;
-        }
-        if (sh.isEmpty()) break;
-        t=sh.cutTagByName("div");
-    }
-    sh.retainTagByName("div");
-    //sh.retainTagByName("div");
-    //sh.retainTagByName("div",2);
-    //sh.retainTagByName("div");
-    //t=sh.cutTagByName("div",2);
-    //t.retainTagByName("div");
-    //n = t.cutTagByName("div");
-
-    /*Get status and obmit event 8th*/
-    n.setContent(sh.getBetween("<p class=\"time\">","</p>"));
-    n.trim();
-    parseStatus2(n);
-    set2Value(8,status,minutes);
-    if (DEBUG)
-        cout<<"Status:"<<status<<endl;
-    //t.viewContent();
-    /* Get other infos */
-    sh.retainTagByName("section",3);
-    sh.retainTagByName("div");
-    sh.retainTagByName("div");
-    t=sh.cutTagByName("section");
-    if (t.contain("Scoring Summary")) {
-        if (DEBUG) cout<<"->Scoring Summary"<<endl;
-        int sc[2]={0,0};
-        int sc1[2]={0,0};
-        for (i=1;i<=2;i++)
-        {
-            nh=t.cutTagByName("tbody");
-            n=nh.cutTagByName("tr");
-            while (!n.isEmpty())
-            {
-                n.retainTagByName("td");
-                if (n.contain("'"))
-                {
-                    n.retainBetween("(","'");
-                    if (n.contain(" miss ")) continue;
-                    while (n.contain(" "))
-                        {
-                            n.deleteTo(" ");
-                        }
-                    v[i]=nh.toInt();
-                    sc[i]++;
-                    if (v[i]<46) sc1[i]++;
-                }
-                n=nh.cutTagByName("tr");
-            }
-        }
-        set2Value(0,sc[0],sc[1]);
-        set2Value(1,sc1[0],sc1[1]);
-        t=sh.cutTagByName("section");
-    }
-    if (t.contain("Match Stats")) {
-        if (DEBUG) cout<<"->Match Stats"<<endl;
-        t.retainTagByName("table");
-        n=t.cutTagByName("tr");
-        //if (DEBUG) n.viewContent();
-        if (n.contain("Shots"))
-        {
-            for (i=0;i<2;i++)
-            {
-                nh=n.cutTagByName("td",i+1);
-                nh.trim();
-                //if (DEBUG) nh.viewContent();
-                if (nh.contain("-")) v[i]=0;
-                else v[i]=nh.toInt();
-                //setValue(4,v,i);
-                nh.retainBetween("(",")");
-                g[i]=nh.toInt();
-                //setValue(5,v,i);
-            }
-            set2Value(4,v[0],v[1]);
-            set2Value(5,g[0],g[1]);
-            n=t.cutTagByName("tr");
-        }
-        if (n.contain("Fouls"))
-        {
-            n=t.cutTagByName("tr");
-        }
-        if (n.contain("Corner Kicks"))
-        {
-            for (i=0;i<2;i++)
-            {
-                nh=n.cutTagByName("td",i+1);
-                nh.trim();
-                if (nh.contain("-")) v[i]=0;
-                else v[i]=nh.toInt();
-                //setValue(6,v,i);
-            }
-            set2Value(6,v[0],v[1]);
-            n=t.cutTagByName("tr");
-        }
-        if (n.contain("Offsides"))
-        {
-            n=t.cutTagByName("tr");
-        }
-        if (n.contain("Time of Possession"))
-        {
-            for (i=0;i<2;i++)
-            {
-                nh=n.cutTagByName("td",i+1);
-                nh.trim();
-                if (nh.contain("-")) v[i]=0;
-                else v[i]=nh.toInt();
-                //setValue(7,v,i);
-            }
-            set2Value(7,v[0],v[1]);
-            n=t.cutTagByName("tr");
-        }
-        if (n.contain("Yellow Cards"))
-        {
-            for (i=0;i<2;i++)
-            {
-                nh=n.cutTagByName("td",i+1);
-                nh.trim();
-                if (nh.contain("-")) v[i]=0;
-                else v[i]=nh.toInt();
-                if (v[i]>0) reCard=false;
-                //setValue(3,v,i);
-            }
-            set2Value(3,v[0],v[1]);
-            n=t.cutTagByName("tr");
-        }
-        if (n.contain("Red Cards"))
-        {
-            for (i=0;i<2;i++)
-            {
-                nh=n.cutTagByName("td",i+1);
-                nh.trim();
-                if (nh.contain("-")) v[i]=0;
-                else v[i]=nh.toInt();
-                if (reCard) if (v[i]>0) reCard=false;
-                //setValue(2,v,i);
-            }
-            set2Value(2,v[0],v[1]);
-            n=t.cutTagByName("tr");
-        }
-
-        t=sh.cutTagByName("section");
-    }
-    /*
-    if (reCard) {
-        if (DEBUG) cout<<"->Recalcule red & yellow cards"<<endl;
-        for (i=0; i<2; i++)
-        {
-            t.removeTagByName("div",3);
-            //int card[3];
-            v[i]=t.count("soccer-icons-yellowcard");
-            //setValue(3,v,i);
-            g[i]=t.count("soccer-icons-redcard");
-            //setValue(2,v,i);
-            //cout<<"Team "<<i<<" has "<<card[1]<<" yellow card(s) and "<<card[2]<<" red card(s)."<<endl;
-            t=sh.cutTagByName("div");
-        }
-        set2Value(3,v[0],v[1]);
-        set2Value(2,g[0],g[1]);
-    }
     */
     isFirstTime=false;
     //cout<<"End of get data"<<endl;
@@ -844,10 +543,11 @@ int main(int argc, char** argv)
                 break;
             }
         }
-        //DEBUG = true;
-        init_mysql_conf();
-        init_mysql();
-
+        if (!DEBUG)
+        {
+            init_mysql_conf();
+            init_mysql();
+        }
         mid = atoi(argv[1]);
         killSame(mid);
         //setEvent(10);
